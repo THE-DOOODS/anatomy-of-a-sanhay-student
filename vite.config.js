40784ed3cbce7744653ff5cd7f4f5596827a4e5d
GarudaFX <franfra10j@gmail.com>
2024-01-08 00:08:07 +0800

    added pwa

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,45 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			devOptions: {
+				enabled: true,
+			},
+			manifiest: {
+				theme_color: "#bb2e2e",
+				background_color: "#Fff",
+				display: "standalone",
+				scope: "/",
+				start_url: "/",
+				name: "Anatomy of a Sanhay Student",
+				short_name: "AOSS",
+				description: "Know the anatomy of a Sanhay Student",
+				icons: [
+					{
+						src: "/icon-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/icon-256x256.png",
+						sizes: "256x256",
+						type: "image/png",
+					},
+					{
+						src: "/icon-384x384.png",
+						sizes: "384x384",
+						type: "image/png",
+					},
+					{
+						src: "/icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
 });
